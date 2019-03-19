9664af8fbb9fa108a41923154115e35f3c6b88fd
louiseslb <louise.saliba99@gmail.com>
2019-03-19 09:00:39 +0100

    debut fonction

--- lib/codeInterpreter/codeInterpreter.c
+++ lib/codeInterpreter/codeInterpreter.c
@@ -1,1 +1,17 @@
 #include "codeInterpreter.h"
+
+
+float result (Entity e, float x){
+    char * fonction = NULL;
+    float fx = 0;
+if (e == NULL){
+    return 0;
+} else {
+    if (e->token == VARIABLE){
+        e->value =x;
+    }
+    if (e->left_operand !=NULL){
+        fx = result(e->left_operand,x);
+    }
+    return fx;
+}
--- lib/codeInterpreter/codeInterpreter.h
+++ lib/codeInterpreter/codeInterpreter.h
@@ -1,4 +1,24 @@
 #ifndef TB7GRAPHER_CODEINTERPRETER_H
 #define TB7GRAPHER_CODEINTERPRETER_H
 
+
+#include "../tokens.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+float result (Entity e, float x);
+/**
+ * @brief Calcule le f(x) du x donné en paramètre avce la focntion dans l'arbre en paramètre
+ *
+ * @param e l'arbre contenant la fonction
+ * @param x la variable
+ * @return f(x)
+ *
+ * @pre l'arbre existe
+ * @post le calcul est valide
+ * @invariant
+ */
+
+
 #endif //TB7GRAPHER_CODEINTERPRETER_H
