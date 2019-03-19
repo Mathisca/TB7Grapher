cdcb091f9665f6a341bd72ee464176d4234b3025
Jérémy Albrecht <ajeremyalbrecht@gmail.com>
2019-03-19 09:00:18 +0100

    add token list

--- lib/tokens.h
+++ lib/tokens.h
@@ -1,7 +1,7 @@
 #ifndef TB7GRAPHER_TOKENS_H
 #define TB7GRAPHER_TOKENS_H
 
-enum TOKEN {
+typedef enum TOKENS {
     NO_TOKEN,
     REAL,
     OPERATOR,
@@ -11,10 +11,9 @@ enum TOKEN {
     PAR_OPN,
     PAR_CLS,
     VARIABLE
-};
-typedef enum TOKEN TOKEN;
+} TOKENS;
 
-enum FUNCTIONS {
+typedef enum FUNCTIONS {
     SIN,
     COS,
     TAN,
@@ -24,28 +23,57 @@ enum FUNCTIONS {
     LOG,
     LN
 
-};
-typedef enum FUNCTIONS FUNCTIONS;
+} FUNCTIONS;
 
-enum OPERATORS{
+typedef enum OPERATORS{
     MULTIPLY,
     DIVIDE,
     PLUS,
     MINUS,
     POWER
-};
-typedef enum OPERATORS OPERATORS;
+} OPERATORS;
 
+typedef enum ERRORS{
+    DIV_BY_ZERO,
+    NON_REAL_OPERATION,
+    UNRECOGNIZED_CHAR,
+    UNRECOGNIZED_FUNCTION,
+    SYNTAX_ERROR
+} ERRORS;
+
+typedef union unionSt {
+    double real;
+    OPERATORS operators;
+    FUNCTIONS functions;
+    ERRORS error;
+} Union;
+
+typedef struct elementSt {
+    TOKENS token;
+    Union value;
+} Element;
 
 typedef struct entitySt * Entity;
 
 struct entitySt {
-    TOKEN token;
-    double value;
+    Element element;
 
     Entity left_operand;
     Entity right_operand;
 };
 
+typedef struct pointSt * Point;
+struct pointST {
+    double x;
+    double y;
+
+    Point nextPoint;
+};
+
+typedef struct elementListSt * ElementList;
+struct elementListSt {
+    Element element;
+    ElementList nextElement;
+};
 
 #endif //TB7GRAPHER_TOKENS_H
