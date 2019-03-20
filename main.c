c38edb41cafd8864046407626c3f24979f2aef6b
louiseslb <louise.saliba99@gmail.com>
2019-03-20 08:24:53 +0100

    debut fonction

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 int main() {
     initGraphics();
     startMainLoop();
+    ElementList list = createMockListTrue();
+    Entity tree = createTree(list);
+    Result r = result(tree, 6);
+    double res = r.value;
+    printf("%lf", res);
 
     return 0;
 }
