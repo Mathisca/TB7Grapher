f1606f9271eea7a3623a21c9631df22f78a120b0
louiseslb <louise.saliba99@gmail.com>
2019-03-20 13:27:38 +0100

    debut fonction

--- lib/codeInterpreter/codeInterpreter.c
+++ lib/codeInterpreter/codeInterpreter.c
@@ -2,82 +2,82 @@
 
 
 Result result(Entity e, double x) {
-    Result r; // structure à retourner
-    r.value = 0; //initialisation de la structure à retourner
-    r.error = NO_ERROR;
+    Result r; // structure to return
+    r.value = 0; //initializing the structure to return (type of error and value)
+    r.error = NO_ERROR; //initializing the error type of the structure
 
-    if (e == NULL || e->element.token == ERROR) { // cas d'arrêt
-        r.error = NO_INPUT;
-        return r;
+    if (e == NULL || e->element.token == ERROR) { // break case
+        r.error = NO_INPUT; // if the tree is empty, or if there is previous errors (syntax...) assigning the type of error : NO_INPUT
+        return r; // returns a structure with an error type and 0 as a value
     } else {
 
-        if (e->element.token == VARIABLE) { //définition de la variable x
-            e->element.value.real = x;
+        if (e->element.token == VARIABLE) { //if there is a variable
+            e->element.value.real = x; //assigning the value x to the variable
         }
 
-        if (e->right_operand == NULL && e->left_operand == NULL) { // si l'arbre n'a pas de fils
-            r.value = e->element.value.real;
+        if (e->right_operand == NULL && e->left_operand == NULL) { // if the tree doesn't have sons
+            r.value = e->element.value.real; //the value of the result is the value in the node
         } else {
-            Result left = result(e->left_operand, x); //vérification des erreurs
-            if (left.error != NO_ERROR) {
-                r.error = left.error;
-                r.value = 0;
+            Result left = result(e->left_operand, x); //checking for errors
+            if (left.error != NO_ERROR) { //if there is an error
+                r.error = left.error; //assigning the error to the result
+                r.value = 0; //if there is an error, value of the result is 0
                 return r;
             }
-            result(e->left_operand, x);
+            result(e->left_operand, x); //calling the function on the left son
         }
 
-        if (e->element.token == OPERATOR) { //gestion des opérations (*, /, +, -)
-            switch (e->element.value.operators) {
+        if (e->element.token == OPERATOR) { //management of the operators (+, -, *, /)
+            switch (e->element.value.operators) { //in case the type of the node is an operator
                 case MULTIPLY:
-                    r.value = result(e->left_operand, x).value * result(e->right_operand, x).value;
+                    r.value = result(e->left_operand, x).value * result(e->right_operand, x).value; //multiplying left and right sons
                     break;
                 case DIVIDE:
-                    if (e->left_operand->element.value.real == 0) { //valeur interdite
-                        r.error = DIV_BY_ZERO; //type d'erreur
+                    if (e->left_operand->element.value.real == 0) { //non authorized value
+                        r.error = DIV_BY_ZERO; //changing the error type : division by zero
                     }
-                    r.value = result(e->left_operand, x).value / result(e->right_operand, x).value;
+                    r.value = result(e->left_operand, x).value / result(e->right_operand, x).value; //dividing left son by right son
                     break;
                 case PLUS:
-                    r.value = result(e->left_operand, x).value + result(e->right_operand, x).value;
+                    r.value = result(e->left_operand, x).value + result(e->right_operand, x).value; //additionning left and right sons
                     break;
                 case MINUS:
-                    r.value = result(e->left_operand, x).value - result(e->right_operand, x).value;
+                    r.value = result(e->left_operand, x).value - result(e->right_operand, x).value; //left son minus right son
                     break;
                 case POWER:
-                    r.value = pow((result(e->left_operand, x).value), (result(e->right_operand, x).value));
+                    r.value = pow((result(e->left_operand, x).value), (result(e->right_operand, x).value)); //left son power right son
             }
-        } else if (e->element.token == FUNCTION) { //gestion des fonctions
-            switch (e->element.value.functions) {
+        } else if (e->element.token == FUNCTION) { //management of the functions
+            switch (e->element.value.functions) { //in case the type of the node is a function
                 case SIN:
-                    r.value = sin(result(e->left_operand, x).value);
+                    r.value = sin(result(e->left_operand, x).value); // function(left son)
                     break;
                 case COS:
                     r.value = cos(result(e->left_operand, x).value);
                     break;
                 case TAN:
-                    if (e->left_operand->element.value.real == M_2_PI) {
-                        r.error = NON_REAL_OPERATION;
+                    if (e->left_operand->element.value.real == M_2_PI) { //non authorized value
+                        r.error = NON_REAL_OPERATION; //changing the type of error : non authorized operation
                     }
                     r.value = tan(result(e->left_operand, x).value);
                     break;
                 case SINC:
-                    if (e->left_operand->element.value.real == 0) {
-                        r.error = DIV_BY_ZERO;
+                    if (e->left_operand->element.value.real == 0) { //non authorized value
+                        r.error = DIV_BY_ZERO; //changing the type of error : division by zero
                     }
-                    r.value = (sin(result(e->left_operand, x).value) / result(e->left_operand, x).value);
+                    r.value = (sin(result(e->left_operand, x).value) / result(e->left_operand, x).value); // definition of the function
                     break;
                 case COSC:
-                    if (e->left_operand->element.value.real == 0) {
-                        r.error = DIV_BY_ZERO;
+                    if (e->left_operand->element.value.real == 0) { //non authorized value
+                        r.error = DIV_BY_ZERO; //changing the type of error : division by zero
                     }
-                    r.value = (cos(result(e->left_operand, x).value) / result(e->left_operand, x).value);
+                    r.value = (cos(result(e->left_operand, x).value) / result(e->left_operand, x).value); // definition of the function
                     break;
                 case TANC:
-                    if (e->left_operand->element.value.real == 0) {
-                        r.error = DIV_BY_ZERO;
+                    if (e->left_operand->element.value.real == 0) { //non authorized value
+                        r.error = DIV_BY_ZERO; //changing the type of error : division by zero
                     }
-                    r.value = (tan(result(e->left_operand, x).value) / result(e->left_operand, x).value);
+                    r.value = (tan(result(e->left_operand, x).value) / result(e->left_operand, x).value); // definition of the function
                     break;
                 case ABS:
                     r.value = fabs(result(e->left_operand, x).value);
@@ -86,14 +86,14 @@ Result result(Entity e, double x) {
                     r.value = exp(result(e->left_operand, x).value);
                     break;
                 case LOG:
-                    if (e->left_operand->element.value.real <= 0) {
-                        r.error = NON_REAL_OPERATION;
+                    if (e->left_operand->element.value.real <= 0) { //non authorized value
+                        r.error = NON_REAL_OPERATION; //changing the type of error : non authorized operation
                     }
                     r.value = log10(result(e->left_operand, x).value);
                     break;
                 case LN:
-                    if (e->left_operand->element.value.real <= 0) {
-                        r.error = NON_REAL_OPERATION;
+                    if (e->left_operand->element.value.real <= 0) { //non authorized value
+                        r.error = NON_REAL_OPERATION; //changing the type of error : non authorized operation
                     }
                     r.value = log(result(e->left_operand, x).value);
                     break;
@@ -107,31 +107,31 @@ Result result(Entity e, double x) {
                     r.value = tanh(result(e->left_operand, x).value);
                     break;
                 case SQRT:
-                    if (e->left_operand->element.value.real < 0) {
-                        r.error = NON_REAL_OPERATION;
+                    if (e->left_operand->element.value.real < 0) { //non authorized value
+                        r.error = NON_REAL_OPERATION; //changing the type of error : non authorized operation
                     }
                     r.value = sqrt(result(e->left_operand, x).value);
                     break;
                 case ARCSIN:
-                    if (e->left_operand->element.value.real < -1 || e->left_operand->element.value.real > 1) {
-                        r.error = NON_REAL_OPERATION;
+                    if (e->left_operand->element.value.real < -1 || e->left_operand->element.value.real > 1) { //non authorized values
+                        r.error = NON_REAL_OPERATION; //changing the type of error : non authorized operation
                     }
                     r.value = asin(result(e->left_operand, x).value);
                     break;
                 case ARCCOS:
-                    if (e->left_operand->element.value.real < -1 || e->left_operand->element.value.real > 1) {
-                        r.error = NON_REAL_OPERATION;
+                    if (e->left_operand->element.value.real < -1 || e->left_operand->element.value.real > 1) { //non authorized values
+                        r.error = NON_REAL_OPERATION; //changing the type of error : non authorized operation
                     }
                     r.value = acos(result(e->left_operand, x).value);
                     break;
                 case ARCTAN:
-                    if (e->left_operand->element.value.real < -1 || e->left_operand->element.value.real > 1) {
-                        r.error = NON_REAL_OPERATION;
+                    if (e->left_operand->element.value.real < -1 || e->left_operand->element.value.real > 1) { //non authorized values
+                        r.error = NON_REAL_OPERATION; //changing the type of error : non authorized operation
                     }
                     r.value = atan(result(e->left_operand, x).value);
                     break;
-                default: //si la fonction ne correspond à aucun cas ci dessus
-                    r.value = 0;
+                default: //if the function matches non of the cases above
+                    r.value = 0; //assigning the value zero to the result because the function hasn't been recognized
                     break;
             }
         }
@@ -141,31 +141,31 @@ Result result(Entity e, double x) {
 }
 
 
-double test_evaluation() { //fonction test du programme --> création arbre
-    double res = 0;
+double test_evaluation() { //test function of the program : creating a tree
+    double res = 0; //Value to return
     Result r;
     Entity e = malloc(sizeof(struct entitySt));
-    e->right_operand = malloc(sizeof(struct entitySt));
+    e->right_operand = malloc(sizeof(struct entitySt)); //allocating memory to the tree
     e->left_operand = malloc(sizeof(struct entitySt));
     e->right_operand->left_operand = malloc(sizeof(struct entitySt));
     e->left_operand->left_operand = malloc(sizeof(struct entitySt));
-    e->element.token = OPERATOR;
-    e->element.value.operators = MULTIPLY;
-    e->right_operand->element.token = FUNCTION;
-    e->right_operand->element.value.functions = LN;
-    e->right_operand->left_operand->element.token = VARIABLE;
-    e->left_operand->element.token = FUNCTION;
-    e->left_operand->element.value.functions = SIN;
-    e->left_operand->left_operand->element.token = REAL;
-    e->left_operand->left_operand->element.value.real = 9;
-    e->left_operand->right_operand = NULL;
+    e->element.token = OPERATOR; //main node is an operator
+    e->element.value.operators = MULTIPLY; // the value of this operator is a multiply
+    e->right_operand->element.token = FUNCTION; //right node is a function
+    e->right_operand->element.value.functions = LN; // value of this function
+    e->right_operand->left_operand->element.token = VARIABLE; // left node of the function is a variable
+    e->left_operand->element.token = FUNCTION; //left node is a function
+    e->left_operand->element.value.functions = SIN; // value of this function
+    e->left_operand->left_operand->element.token = REAL; // left node of this function is a real
+    e->left_operand->left_operand->element.value.real = 9; //value of this real
+    e->left_operand->right_operand = NULL; // assigning NULL to every sons of node without sons
     e->left_operand->left_operand->left_operand = NULL;
     e->left_operand->left_operand->right_operand = NULL;
     e->right_operand->right_operand = NULL;
     e->right_operand->left_operand->right_operand = NULL;
     e->right_operand->left_operand->left_operand = NULL;
 
-    r = result(e, 0.5);
-    res = r.value;
+    r = result(e, 0.5); // calling the function that calculates f(x)
+    res = r.value; //assigning the correct value to the retruned value
     return res;
 }
